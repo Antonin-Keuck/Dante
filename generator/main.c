9ab24b13f93051db61b42fa2936c9ac186844054
Antonin Keuck <antonin.keuck@epitech.eu>
2019-07-09 11:30:48 +0100

    [ADD] -h informations on how to use the binary

--- generator/main.c
+++ generator/main.c
@@ -6,13 +6,20 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 #include "dante_gen.h"
 
 int main(int ac, char *av[])
 {
-    if (ac < 3 || ac > 4) {
-        fprintf(stderr, "Wrong number of arguements\n");
+    if (ac != 3) {
+        if (ac == 2 && strcmp("-h", av[1]) == 0) {
+            printf("Usage: ./generator [width] [height]\n Generate a maze map\n");
+            return (0);
+        }
+        fprintf(stderr, "Wrong number of arguements\nFor more information please try again with -h\n");
         return (84);
+    } else {
+        printf("marche pas ... '%s\n'", av[1]);
     }
     if (error_gestion(av) != 0 || dante_gen(av) != 0) {
         return (84);
--- solver/main.c
+++ solver/main.c
@@ -10,13 +10,19 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <string.h>
 #include <unistd.h>
 #include "solver_list.h"
 #include "dante_solver.h"
 
 int main(int ac, char **av)
 {
-    if (ac == 2 && solver(av) == 0)
+    if (ac != 2) {
+        fprintf(stderr, "Wrong number of arguements\nFor more information please try again with -h\n");
+    } else if (strcmp("-h", av[1]) == 0) {
+        printf("Usage: ./solver [maze_filepath]\n Solve and find the shortest way of a maze\n");
+        return (0);
+    } else if (solver(av) == 0)
         return (0);
     return (84);
 }
